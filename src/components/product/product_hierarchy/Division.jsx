7a8a7194dbdab15671f575d92b1fa03c41c8a69c
Bharani <bharaniraju742@gmail.com>
2024-08-17 19:47:36 +0530

    Added section,department and division

--- src/components/product/product_hierarchy/Division.jsx
+++ src/components/product/product_hierarchy/Division.jsx
@@ -1,8 +1,143 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { B2B_API } from '../../../api/Interceptor';
+import '../../../css/formstyles/Formstyles.css';
+import B2BButton from '../../../common/B2BButton';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Division = () => {
+  const initialState = {
+    divisionId: "",
+    name: "",
+    image: "",
+    description: "",
+    createdDate: "",
+    status: "ACTIVE"
+  }
+
+  const [division, setDivision] = useState(initialState);
+  const [divisions, setDivisions] = useState([]);
+  const navigate = useNavigate();
+
+
+  useEffect(() => {
+    fetchDivision();
+  }, [])
+
+  const fetchDivision = async () => {
+    try {
+      const res = await B2B_API.get(`division`).json();
+      setDivisions(res);
+      console.log(res, "div");
+
+    }
+    catch (error) {
+      console.error("Failed to Fetch  Division", error)
+    }
+  }
+  const addDivision = async (event) => {
+    try {
+      event?.preventDefault();
+      const value = await B2B_API.post(`division`, { json: division }).json();
+      fetchDivision();
+      setDivision(initialState);
+    }
+    catch (error) {
+      console.error("Failed to Add Division")
+    }
+  }
+
+  const handleChange = (event, key) => {
+    setDivision((prev) => (
+      { ...prev, [key]: event.target.value }
+    ))
+  }
+
+
+  const json = [
+    {
+      label: "Division Id",
+      value: division.divisionId,
+      onChange: (event) => handleChange(event, "divisionId"),
+      style: { cursor: 'not-allowed' },
+      disabled: true,
+      type: "text",
+      placeholder: "Division ID"
+    },
+    {
+      label: "Division Name",
+      value: division.name,
+      required: true,
+      onChange: (event) => handleChange(event, "name"),
+      type: "text",
+      placeholder: "Division Name"
+    },
+    {
+      label: "Description",
+      value: division.description,
+      onChange: (event) => handleChange(event, "description"),
+      type: "text",
+      placeholder: "Description"
+    },
+    {
+      label: "Status",
+      value: division.status,
+      onChange: (event) => handleChange(event, "status"),
+      type: "radio",
+      className: "form-group status-container",
+      placeholder: "Status",
+      options: [
+        { value: "ACTIVE", label: "ACTIVE" },
+        { value: "INACTIVE", label: "INACTIVE" }
+      ]
+    }
+  ]
+
   return (
-    <div>Division</div>
+    <div className='grid-container'>
+      <form onSubmit={(event) => addDivision(event)} className='form-container'>
+        {json.map(e => (
+          <div className={e.className ? e.className : "form-group"}>
+            <label className='form-label'>{e.label}</label>
+            {e.type == "radio" ? (
+              <div className='radio-label-block'>
+                {e.options.map((option, idx) => (
+                  <div className='radio-group'>
+                    <div className='status-block'>
+                      <input
+                        id={`${e.name}-${option.value.toLowerCase()}`}
+                        value={option.value}
+                        style={e.style && e.style}
+                        onChange={(event) => e.onChange(event, e.name)}
+                        checked={e.value === option.value}
+                        type={e.type}
+                        placeholder={e.placeholder}
+                      />
+                      <label className='form-span radio' htmlFor={`${e.name}-${option.value.toLowerCase()}`}>{option.label}</label>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )
+              : <input
+                value={e.value}
+                className='form-input'
+                style={e.style}
+                disabled={e.disabled}
+                onChange={e.onChange}
+                type={e.type}
+                required={e.required}
+                placeholder={e.placeholder}
+              />
+            }
+
+          </div>
+        ))}
+        <div className='save-button-container'>
+          <B2BButton type='submit' name="Save & New" />
+          <B2BButton type='submit' name="Save"/>
+        </div>
+      </form>
+    </div>
   )
 }
 
--- src/components/product/product_hierarchy/Section.jsx
+++ src/components/product/product_hierarchy/Section.jsx
@@ -1,8 +1,172 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { B2B_API } from '../../../api/Interceptor';
+import B2BSelect from '../../../common/B2BSelect';
+import B2BButton from '../../../common/B2BButton';
 
 const Section = () => {
+
+  const initialData = {
+    sectionId: "",
+    name: "",
+    description: "",
+    createdDate: "",
+    status: "ACTIVE",
+    departmentId: ""
+  }
+
+  const [section, setSection] = useState(initialData);
+  const [sections, setSections] = useState([]);
+  const [departments, setDepartments] = useState([]);
+
+  useEffect(() => {
+    getAllSection();
+    getAllDepartment();
+  }, [])
+
+  const getAllSection = async () => {
+    try {
+      const res = await B2B_API.get(`section`).json();
+      setSections(res);
+    } catch (err) {
+      console.error("Failed to Fetch Section", err)
+    }
+  }
+
+  const addSection = async (event) => {
+    event.preventDefault();
+    try {
+      const res = await B2B_API.post(`section`, { json: section }).json();
+      getAllSection();
+      setSection(initialData);
+    } catch (err) {
+      console.error("Failed to add Section", err)
+    }
+  }
+
+  const handleChange = (event, key) => {
+    setSection((prev) => ({
+      ...prev, [key]: key === "departmentId" ? event : event.target?.value
+    }))
+  }
+
+  const getAllDepartment = async () => {
+    try {
+      const res = await B2B_API.get(`department`).json();
+      setDepartments(res.response);
+    } catch (err) {
+      console.error("Failed to Fetch Department")
+    }
+  }
+
+  const json = [
+    {
+      label: "Section Id",
+      value: section.sectionId,
+      onChange: (event) => handleChange(event, "SectionId"),
+      style: { cursor: 'not-allowed' },
+      disabled: true,
+      type: "text",
+      placeholder: "Section ID"
+    },
+    {
+      label: "Section Name",
+      value: section.name,
+      required: true,
+      onChange: (event) => handleChange(event, "name"),
+      type: "text",
+      placeholder: "Section Name"
+    },
+    {
+      label: "Description",
+      value: section.description,
+      onChange: (event) => handleChange(event, "description"),
+      type: "text",
+      placeholder: "Description"
+    },
+    {
+      label: "Status",
+      value: section.status,
+      onChange: (event) => handleChange(event, "status"),
+      type: "radio",
+      className: "form-group status-container",
+      placeholder: "Status",
+      options: [
+        { value: "ACTIVE", label: "ACTIVE" },
+        { value: "INACTIVE", label: "INACTIVE" }
+      ]
+    },
+    {
+      label: "Department",
+      value: departments.departmentId,
+      onChange: (event) => handleChange(event, "departmentId"),
+      type: "select",
+      placeholder: "Department"
+    }
+  ]
+
+  const renderContent = (e) => {
+    switch (e.type) {
+      case "text":
+        return <input
+          value={e.value}
+          className='form-input'
+          style={e.style}
+          disabled={e.disabled}
+          onChange={e.onChange}
+          type={e.type}
+          data={departments.map(d => d?.name)}
+          required={e.required}
+          placeholder={e.placeholder}
+        />
+
+      case "radio":
+        return <div className='radio-label-block'>
+          {e.options.map((option) => (
+            <div className='radio-group'>
+              <div className='status-block'>
+                <input
+                  id={`${e.name}-${option.value.toLowerCase()}`}
+                  value={option.value}
+                  style={e.style && e.style}
+                  onChange={(event) => e.onChange(event, e.name)}
+                  checked={e.value === option.value}
+                  type={e.type}
+                  placeholder={e.placeholder}
+                />
+                <label className='form-span radio' htmlFor={`${e.name}-${option.value.toLowerCase()}`}>{option.label}</label>
+              </div>
+            </div>
+          ))}
+        </div>
+
+      case "select":
+        return <B2BSelect
+          styles={{ option: { fontSize: '13px' }, input: { fontSize: '13px' } }}
+          value={section.departmentId}
+          data={departments.map(r => ({ label: r?.name, value: r.departmentId }))}
+          required={true}
+          clearable={departments.length > 0 ? true : false}
+          onChange={(value) => handleChange(value, "departmentId")}
+          placeholder={"Select DepartmentId"}
+        />
+    }
+  }
+
   return (
-    <div>Section</div>
+    <div className='grid-container'>
+      <form onSubmit={(event) => addSection(event)} className='form-container'>
+        {json.map(e => (
+          <div className={e.className ? e.className : "form-group"}>
+            <label className='form-label'>{e.label}</label>
+            {renderContent(e)}
+          </div>
+        ))}
+        <div className='save-button-container'>
+          <B2BButton type='submit' name="Save & New" />
+          <B2BButton type='submit' name="Save" />
+        </div>
+      </form>
+    </div>
   )
 }
 
